f8c12c736bc9259d1f5bab0b645689346f03d460
Aiden Thacker <aidenthacker86@gmail.com>
2024-01-28 13:19:47 -0600

    Made Prefabs for Apple, Saloon Door, and Wooden Door

---
+++ Assets/Scripts/Apple/Apple.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    public GameObject appleText;
+    
+    private bool isInRange = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInRange == true)
+        {
+            appleText.SetActive(true);
+        }
+        if (isInRange == false)
+        {
+            appleText.SetActive(false);
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Play Audio here?? :\
+            isInRange = true;
+            Debug.Log("Fuck'Em thats why");
+            if (Input.GetMouseButton(0))
+            {
+                appleText.SetActive(false);
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            isInRange = false;
+        }
+    }
+}
